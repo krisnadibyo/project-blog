3c7e527c669d68f097282d633af7cf4cde78ace0
Krisna Dibyo <krisna_itbstei@hotmail.com>
2024-10-12 22:23:27 +0700

    darkmode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import clsx from 'clsx';
 import { Rss, Sun, Moon } from 'react-feather';
@@ -6,8 +7,39 @@ import Logo from '@/components/Logo';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './Header.module.css';
+import Cookies from 'js-cookie';
+import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
 
-function Header({ theme, className, ...delegated }) {
+function Header({ initialTheme, className, ...delegated }) {
+  const [theme, setTheme] = React.useState(initialTheme)
+
+  const flipTheme = () => {
+    const nextTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(nextTheme);
+    Cookies.set('color-theme', nextTheme);
+    
+    const COLORS = nextTheme == 'light' ? LIGHT_TOKENS : DARK_TOKENS;
+    const root = document.documentElement;
+
+    root.setAttribute(
+      'data-color-theme',
+      nextTheme
+    );
+    for (const property in COLORS) {
+      if (COLORS.hasOwnProperty(property)) {
+          // For CSS variables (custom properties), use setProperty
+          if (property.startsWith('--')) {
+              root.style.setProperty(property, COLORS[property]);
+          } else {
+              // Otherwise, apply regular styles
+              root.style[property] = COLORS[property];
+          }
+      }
+  }
+
+  }
+
+
   return (
     <header
       className={clsx(styles.wrapper, className)}
@@ -28,8 +60,10 @@ function Header({ theme, className, ...delegated }) {
             View RSS feed
           </VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
+        <button className={styles.action}
+          onClick={flipTheme}
+        >
+          {theme === 'light' ? <Moon size="1.5rem" /> : <Sun size="1.5rem" />}
           <VisuallyHidden>
             Toggle dark / light mode
           </VisuallyHidden>
